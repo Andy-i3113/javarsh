bb17c276b9acb25b91b196339a4a2dd4838fe97f
Andrew <interwebandy@gmail.com>
2021-05-09 18:04:39 +0300

    com.javarush.task.task16.task1626;

--- 2.JavaCore/src/com/javarush/task/task16/task1626/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1626/Solution.java
@@ -12,9 +12,27 @@ public class Solution {
         new Thread(new CountUpRunnable(), "Увеличиваем").start();
     }
 
-    public static class CountUpRunnable {
-        //Add your code here - добавь код тут
+    public static class CountUpRunnable implements Runnable{
+        private int countIndexUp = 1;
+
+        public void run() {
+            try {
+                while (true) {
+                    System.out.println(toString());
+                    countIndexUp += 1;
+                    Thread.sleep(500);
+                    if (countIndexUp > Solution.number) return;
+
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        public String toString() {
+            return Thread.currentThread().getName() + ": " + countIndexUp;
+        }
     }
+
 
 
     public static class CountdownRunnable implements Runnable {
