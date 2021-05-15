b56fe179f9e91843fb3ab5b3064332ec785f5909
Andrew <interwebandy@gmail.com>
2021-05-15 14:13:29 +0300

    task1610

--- 2.JavaCore/src/com/javarush/task/task16/task1610/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1610/Solution.java
@@ -8,9 +8,11 @@ public class Solution {
     public static void main(String[] args) throws InterruptedException {
         Cat cat1 = new Cat("Мурка");
         Cat cat2 = new Cat("Пушинка");
+
     }
 
-    private static void investigateWorld() {
+    private static void investigateWorld() throws InterruptedException {
+        Thread.sleep(200);
 
     }
 
@@ -18,12 +20,13 @@ public class Solution {
         protected Kitten kitten1;
         protected Kitten kitten2;
 
-        public Cat(String name) {
+        public Cat(String name)  {
             super(name);
             kitten1 = new Kitten("Котенок 1, мама - " + getName());
             kitten2 = new Kitten("Котенок 2, мама - " + getName());
             start();
-        }
+
+              }
 
         public void run() {
             System.out.println(getName() + " родила 2 котенка");
@@ -37,6 +40,8 @@ public class Solution {
         private void initAllKittens() throws InterruptedException {
             kitten1.start();
             kitten2.start();
+            kitten1.join();
+            kitten2.join();
         }
     }
 
@@ -47,7 +52,11 @@ public class Solution {
 
         public void run() {
             System.out.println(getName() + ", вылез из корзинки");
-            investigateWorld();
+            try {
+                investigateWorld();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
