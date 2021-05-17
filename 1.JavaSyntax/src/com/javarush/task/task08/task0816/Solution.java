a889991e3f6ad0b99044939287eccbcaa2751131
Andrey Golubkov <golubkovaa@rushydro.ru>
2021-05-17 14:20:56 +0300

    task0816

--- 1.JavaSyntax/src/com/javarush/task/task08/task0816/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0816/Solution.java
@@ -10,25 +10,25 @@ import java.util.*;
 */
 
 public class Solution {
-    public static HashMap<String, Date> CreateMap() throws ParseException {
-        DateFormat df = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
-        HashMap<String, Date> map = new HashMap<String, Date>();
-        map.put("Stallone1", df.parse("JUNE 1 1981"));
-        map.put("Stallone2", df.parse("OCTOBER 1 1982"));
-        map.put("Stallone3", df.parse("NOVEMBER 1 1983"));
-        map.put("Stallone4", df.parse("JUNE 1 1984"));
-        map.put("Stallone5", df.parse("JUNE 1 1985"));
-        map.put("Stallone6", df.parse("JUNE 1 1986"));
-        map.put("Stallone7", df.parse("JUNE 1 1987"));
-        map.put("Stallone8", df.parse("JUNE 1 1988"));
-        map.put("Stallone9", df.parse("JUNE 1 1989"));
-        map.put("Stallone0", df.parse("JUNE 1 1990"));
+    public static Map<String, Date> createMap() throws ParseException {
+        DateFormat dateFormat = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
+        Map<String, Date> map = new HashMap<>();
+        map.put("Смирнов", dateFormat.parse("MAY 1 2012"));
+        map.put("Stallone1", dateFormat.parse("JUNE 1 1981"));
+        map.put("Stallone2", dateFormat.parse("OCTOBER 1 1982"));
+        map.put("Stallone3", dateFormat.parse("NOVEMBER 1 1983"));
+        map.put("Stallone4", dateFormat.parse("JUNE 1 1984"));
+        map.put("Stallone5", dateFormat.parse("JUNE 1 1985"));
+        map.put("Stallone6", dateFormat.parse("JUNE 1 1986"));
+        map.put("Stallone7", dateFormat.parse("JUNE 1 1987"));
+        map.put("Stallone8", dateFormat.parse("JUNE 1 1988"));
+        map.put("Stallone9", dateFormat.parse("JUNE 1 1989"));
 
-       return map;
+        return map;
+        //напишите тут ваш код
     }
 
-    public static void removeAllSummerPeople(HashMap<String, Date> map) {
-
+    public static void removeAllSummerPeople(Map<String, Date> map) {
         Iterator<Map.Entry<String, Date >> iterator = map.entrySet().iterator();
         while (iterator.hasNext()){
             Map.Entry<String, Date> tmp = iterator.next();
@@ -39,15 +39,9 @@ public class Solution {
         }
     }
 
-    public static void main(String[] args) throws ParseException {
 
-       // HashMap<String, Date> map = CreateMap();
-        removeAllSummerPeople( CreateMap() );
 
+    public static void main(String[] args) {
 
     }
-
-
 }
-
-
