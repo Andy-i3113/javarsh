89a5fb90576130335e88e064751158eec85aac26
Andrew <interwebandy@gmail.com>
2021-05-24 23:08:54 +0300

    task0827-1

--- 1.JavaSyntax/src/com/javarush/task/task08/task0827/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0827/Solution.java
@@ -10,30 +10,22 @@ import java.util.Locale;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
-        System.out.println(isDateOdd("MAY 2 2013"));
+        System.out.println(isDateOdd("MAY 1 2013"));
 
     }
 
-    public static boolean isDateOdd(String date) {
+    public static boolean isDateOdd(String date) throws ParseException {
 
-        boolean flag = true;
-        SimpleDateFormat format1 = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
-
-        try {
+            SimpleDateFormat format1 = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
             Date dateOne = format1.parse(date);
             Date dateTwo = format1.parse("January 1 " + String.valueOf(1900 + dateOne.getYear()));
              // Количество дней между датами в миллисекундах
             long difference = dateOne.getTime() - dateTwo.getTime();
             // Перевод количества дней между датами из миллисекунд в дни
             int days =  (int)(difference / (24 * 60 * 60 * 1000)); // миллисекунды / (24ч * 60мин * 60сек * 1000мс)
-            flag = (days%2 == 0);
+            return (days%2 == 0);
 
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        return flag;
     }
 }
